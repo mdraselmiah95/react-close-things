1d696332e3c1e8fad56f4b2ead7551317304e938
rasel <russelroy15@gmail.com>
2021-09-23 02:16:06 +0600

    WIP on React concept

--- src/components/utilities/fakedb.js
+++ src/components/utilities/fakedb.js
@@ -3,7 +3,12 @@
 const addToDb = (id) => {
   const exists = localStorage.getItem(id);
   console.log(exists);
-  localStorage.setItem(id, 1);
+  if (!exists) {
+    localStorage.setItem(id, 1);
+  } else {
+    const newCount = parseInt(exists) + 1;
+    localStorage.setItem(id, newCount);
+  }
 };
 
 export { addToDb };
