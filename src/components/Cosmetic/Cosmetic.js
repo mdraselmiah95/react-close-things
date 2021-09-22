03ac5e8b467e31e9449fa97162a10e5fe139508a
rasel <russelroy15@gmail.com>
2021-09-23 01:39:43 +0600

    WIP on close react things

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 const Cosmetic = (props) => {
-  console.log(props.cosmetic);
-  return <div></div>;
+  //   console.log(props.cosmetic);
+  const { name, about, address, age, balace, company } = props.cosmetic;
+
+  return (
+    <div>
+      <h2>Name:{name}</h2>
+      <h4>Age: {age}</h4>
+      <h5>Balance: {balace}</h5>
+      <p>{about}</p>
+      <h5>{address}</h5>
+      <h5>{company}</h5>
+    </div>
+  );
 };
 
 export default Cosmetic;
