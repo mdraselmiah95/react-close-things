d4704894dc4ad83a2ee3e5355de7a32b98828bb3
rasel <russelroy15@gmail.com>
2021-09-23 02:22:29 +0600

    WIP on React concept

--- src/components/utilities/fakedb.js
+++ src/components/utilities/fakedb.js
@@ -1,13 +1,24 @@
 //use local storage as your db for now
 
 const addToDb = (id) => {
-  const exists = localStorage.getItem(id);
-  console.log(exists);
+  const exists = localStorage.getItem("shopping_cart");
+  // console.log(exists);
+  let shopping_cart = {};
+
   if (!exists) {
-    localStorage.setItem(id, 1);
+    shopping_cart[id] = 1;
+    // localStorage.setItem(id, 1);
   } else {
-    const newCount = parseInt(exists) + 1;
-    localStorage.setItem(id, newCount);
+    // const newCount = parseInt(exists) + 1;
+    // localStorage.setItem(id, newCount);
+
+    shopping_cart = JSON.parse(exists);
+    if (shopping_cart[id]) {
+      const newCount = shopping_cart[id] + 1;
+      shopping_cart[id] = newCount;
+    } else {
+      shopping_cart[id] = 1;
+    }
   }
 };
 
