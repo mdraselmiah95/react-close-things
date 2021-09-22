a933d1d123d08c60f6285176517c971fcd92f5a9
rasel <russelroy15@gmail.com>
2021-09-23 02:06:11 +0600

    WIP on close react things

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -3,16 +3,21 @@ import React from "react";
 import "./Cosmetic.css";
 const Cosmetic = (props) => {
   //   console.log(props.cosmetic);
-  const { name, about, address, age, balace, company } = props.cosmetic;
-
+  const { name, about, address, age, balace, company, _id } = props.cosmetic;
+  const handlePurchase = (id) => {
+    //set to local storage
+    console.log(id);
+  };
   return (
     <div className="Cosmetic">
       <h2>Name:{name}</h2>
+      <p>_id: {_id}</p>
       <h4>Age: {age}</h4>
       <h5>Balance: {balace}</h5>
       <p>{about.slice(0, 150)}</p>
       <h5>{address}</h5>
       <h5>{company}</h5>
+      <button onClick={handlePurchase(_id)}>Purchase</button>
     </div>
   );
 };
