763670d1c529334d94eed90d502fb78e8fffedd7
rasel <russelroy15@gmail.com>
2021-09-23 01:44:47 +0600

    WIP on close react things

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+// import "./Cosmetic.css";
+import "./Cosmetic.css";
 const Cosmetic = (props) => {
   //   console.log(props.cosmetic);
   const { name, about, address, age, balace, company } = props.cosmetic;
 
   return (
-    <div>
+    <div className="Cosmetic">
       <h2>Name:{name}</h2>
       <h4>Age: {age}</h4>
       <h5>Balance: {balace}</h5>
